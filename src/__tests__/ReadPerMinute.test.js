df5d5801b33948d8764dc443e20e04cd92f2778a
untemps <6Mv2^2S6$2>
2021-01-21 00:08:31 +0100

    test: Fix unit tests

--- src/__tests__/ReadPerMinute.test.js
+++ src/__tests__/ReadPerMinute.test.js
@@ -1,5 +1,76 @@
 import ReadingTimer from '../ReadPerMinute'
 
+const WORDS = [
+	'year',
+	'learn',
+	'key',
+	'cook',
+	'crosswalk',
+	'script',
+	'chief',
+	'plan',
+	'meat',
+	'vague',
+	'demonstrator',
+	'dictionary',
+	'visible',
+	'bomber',
+	'variation',
+	'leader',
+	'channel',
+	'litigation',
+	'royalty',
+	'impulse',
+	'package',
+	'oppose',
+	'privilege',
+	'begin',
+	'operation',
+	'herd',
+	'hemisphere',
+	'incongruous',
+	'horror',
+	'pipe',
+	'start',
+	'vertical',
+	'worry',
+	'reform',
+	'unlike',
+	'exhibition',
+	'disagree',
+	'allow',
+	'patrol',
+	'combine',
+	'impress',
+	'invisible',
+	'cage',
+	'log',
+	'snow',
+	'undertake',
+	'division',
+	'ethics',
+	'damage',
+	'responsible',
+]
+const generateTokenizedText = (separator = '%', maxWordCount = 50, minWordCount = 10) => {
+	let str = ''
+	let int = ''
+	let tok = {}
+	const l = Math.ceil(Math.random() * (maxWordCount - minWordCount)) + minWordCount
+	for (let i = 0; i < l; i++) {
+		const r = Math.random() > 0.7
+		const w = WORDS[Math.floor(Math.random() * WORDS.length)]
+		str += `${i > 0 ? ' ' : ''}${r ? separator : ''}${w}${r ? separator : ''}`
+		if (r) {
+			tok[w] = 'gag'
+			int += `${i > 0 ? ' ' : ''}${r ? tok[w] : ''}`
+		} else {
+			int += `${i > 0 ? ' ' : ''}${r ? separator : ''}${w}${r ? separator : ''}`
+		}
+	}
+	return { str, int, tok }
+}
+
 describe('ReadPerMinute', () => {
 	describe('constructor', () => {
 		it('Instantiates class with no error', () => {
@@ -31,7 +102,7 @@ describe('ReadPerMinute', () => {
 			const instance = new ReadingTimer()
 			expect(instance.parse()).toEqual({ time: 1, words: 1, rate: ReadingTimer.rates.en })
 			expect(
-				instance.parse(generateTokenizedText(null, ReadingTimer.rates.en, ReadingTimer.rates.en).str)
+				instance.parse(generateTokenizedText(null, ReadingTimer.rates.en, ReadingTimer.rates.en).str),
 			).toEqual({ time: 1, words: ReadingTimer.rates.en, rate: ReadingTimer.rates.en })
 		})
 
